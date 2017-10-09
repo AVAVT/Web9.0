a009b23ee0a997abb5df9e805130a4a501d631fb
Long <darknesszx@gmail.com>
2017-10-10 00:14:10 +0700

    Move html/css folder

--- SaaS/app.js
+++ SaaS/app.js
@@ -5,24 +5,15 @@ const outputFileName = "test.json";
 
 let app = express();
 app.get('/', (req, res) => {
-  console.log(__dirname);
-  res.sendFile(path.join(__dirname,"../index.html"));
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get('/about', (req, res) => {
-  res.send("I'm Long");
+  res.send("Web 9.0");
 });
 
 app.get('/question', (req, res) => {
   res.send("Question");
-});
-
-app.get('/testhtml', (req, res) => {
-  let test = 5;
-  let es5String = "abc " + test.toString() + "adas";
-  let es6String = `abc + ${test} + "adas`;
-  console.log(es6String);
-  res.send("<h1>Test html</h1><ul><li>1</li><li>2</li></ul>");
 });
 
 app.get('/style.css', (req, res) => {
