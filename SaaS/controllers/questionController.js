852e6b4935d836347625c1a8747ad16914ec608e
Long <darknesszx@gmail.com>
2017-10-24 17:31:03 +0700

    Update api like

--- SaaS/controllers/questionController.js
+++ SaaS/controllers/questionController.js
@@ -2,57 +2,69 @@ const fileController = require('./fileController');
 const outputFileName = "question.json";
 const questionModel = require('../models/questionModel');
 
-const getQuestionList = () => {
-  let questionList = fileController.readDataFromFile(outputFileName);
-  return questionList;
-}
-
-const saveQuestionList = (questionList) => {
-  fileController.writeDataToFile(outputFileName, questionList);
-}
-
 const getQuestionById = (id) => {
-  // questionModel.findOne({ _id : id }, (err, doc) => {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(doc);
-  //   }
-  // });
-
   return questionModel.findOne({_id : id});
 }
 
-const getRandomQuestion = () => {
-  let questionList = getQuestionList();
-  let randomId = Math.floor(Math.random() * questionList.length);
-  let question = questionList[randomId];
-  question.id =randomId;
-
-  return question;
+const getRandomQuestion = (callback) => {
+  questionModel.count().exec((err, count) => {
+    let randCount = Math.floor(Math.random() * count);
+    questionModel.findOne({}).skip(randCount).exec((err, question) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, question);
+      }
+    });
+  });
 }
 
-const updateQuestion = (id, answer) => {
-  let questionList = getQuestionList();
+const updateQuestion = (id, answer, callback) => {
+  questionModel.findOne({ _id : id }, (err, question) => {
+    console.log(question);
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      console.log("answer ",answer);
+      if (answer === "yes") {
+        question.yes = question.yes + 1;
+      } else if (answer === "no") {
+        question.no = question.no + 1;
+      }
+      question.save((err, doc) => {
+        console.log("updated ",doc);
+        callback(null, doc);
+      });
 
-  if (answer === "yes") {
-    questionList[id].yes += 1;
-  } else if (answer === "no") {
-    questionList[id].no += 1;
-  }
-
-  saveQuestionList(questionList);
+    }
+  })
 }
 
 const addNewQuestion = (question, callback) => {
-  let newQuestion = { question }
+  let newQuestion = { question };
   questionModel.create(newQuestion, (err, doc) => {
     if (err) {
+      console.log(err);
       callback(err);
     } else {
       callback(null, doc);
     }
   });
+}
+
+const updateLikeQuestion = (id, callback) => {
+  questionModel.findOne({ _id : id }, (err, question) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      question.likes += 1;
+      question.save((err, doc) => {
+        callback(null, question);
+      });
+    }
+  })
 }
 
 module.exports = {
@@ -61,5 +73,6 @@ module.exports = {
   getQuestionById,
   getRandomQuestion,
   updateQuestion,
-  addNewQuestion
+  addNewQuestion,
+  updateLikeQuestion
 }
--- SaaS/router/questionRouter.js
+++ SaaS/router/questionRouter.js
@@ -1,61 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const fileController = require('../controllers/fileController');
 const questionController = require('../controllers/questionController');
 
 router.get('/', (req, res) => {
-  let question = questionController.getRandomQuestion();
-
-  res.render("question",
-    {
-      id: question.id,
-      question: question.question,
-      yes: question.yes,
-      no: question.no
-    });
+  questionController.getRandomQuestion((err, question) => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      res.render("question", question);
+    }
+  });
 });
-
-router.get('/test', (req, res) => {
-  testPromise()
-    .then(questionList => res.send(questionList))
-    .catch(err => res.send(err));
-});
-
 
 router.get('/:id', (req, res) => {
   questionController.getQuestionById(req.params.id)
-    .then(doc => { res.render("question", doc); })
+    .then(doc => { res.render("answer", doc); })
     .catch(err => { res.redirect("/"); });
 });
 
-const testPromise = () => {
-  return new Promise(function(resolve, reject) {
-    try {
-      let questionList = fileController.readDataFromFile("test");
-      console.log("resolve");
-      resolve(questionList);
-    }
-    catch (ex) {
-      console.log("reject");
-      reject(ex);
+router.post('/', (req, res) => {
+  questionController.addNewQuestion(req.body.question, (err, question) => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/question/${question.id}`);
     }
   });
-}
-
-router.post('/', (req, res) => {
-  questionController.addNewQuestion(req.body.question, res);
 });
 
 router.post('/:id', (req, res) => {
   let id = req.params.id;
+  let answer = req.body.yes || req.body.no;
 
-  if (req.body.yes) {
-    questionController.updateQuestion(id, "yes");
-  } else if (req.body.no) {
-    questionController.updateQuestion(id, "no");
-  }
-
-  res.redirect(`/question/${id}`);
+  questionController.updateQuestion(id, answer, (err, doc) => {
+    console.log("after update", doc);
+    res.redirect(`/question/${doc._id}`);
+  });
 });
 
 module.exports = router;
