ac398933a01a2732aee4034793244bb601a244e0
AVAVT <itsavavt@gmail.com>
2017-11-18 23:02:25 +0700

    Masonry

--- TechKidsHotGirl/public/js/home.js
+++ TechKidsHotGirl/public/js/home.js
@@ -14,8 +14,14 @@ function loadNextPage() {
         (data) => {
             isLoading = false;
             pageToLoad += 1;
-            // $element.html() =>> replace content
-            $("#girl_list").append(template({images: data}));
+            
+            var itemHtml = $(template({images: data}));
+            
+            // append html to the list and notify masonry of content changes
+            $("#girl_list").append(itemHtml).masonry('appended', itemHtml);
+            $('#girl_list').imagesLoaded().progress( function() {
+              $('#girl_list').masonry('layout');
+            });
 
             if($('body').height() < $(window).height()) loadNextPage();
         },
@@ -29,6 +35,18 @@ function loadNextPage() {
 function checkEndlessScrolling(){
     if(!isLoading && $(window).height() + $(window).scrollTop() > $('body').height() - $(window).height()/2) loadNextPage();
 }
+
+// Initialize masonry layout
+$('#girl_list').masonry({
+    itemSelector: '.girl_item',
+    columnWidth: '.girl_item',
+    percentPosition: true
+});
+
+// Rearrange items after all <img> finished loading and all height are known
+$('#girl_list').imagesLoaded().progress( function() {
+    $('#girl_list').masonry('layout');
+});
 
 $(window).on('scroll', checkEndlessScrolling);
 loadNextPage();
